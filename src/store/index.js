5a7a0da019c74fbbb4edd3b3c997a318206dd375
60kmlh <764493509@qq.com>
2017-07-24 16:15:47 +0800

    'first'

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from '../reducers'
+
+export default function(initState) {
+  const store = createStore(
+    rootReducer
+    initState,
+    compose(
+      applyMiddleware(thunkMiddleware),
+      window.devToolsExtension ? window.devToolsExtension() : undefined
+    )
+  )
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default
+      store.replaceReducer(nextReducer)
+    })
+  }
+
+  return store
+}  
